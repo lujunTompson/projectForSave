55326804bc02b0b0b0ee730ebb68d991729c5ec3
Thompson Wang <m18916275937@163.com>
2022-12-26 23:38:57 -0600

    fix comment

--- saveProject/saveProject.cpp
+++ saveProject/saveProject.cpp
@@ -1,4 +1,4 @@
-﻿// saveProject.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// saveProject.cpp : This file contains the "main" function. Program execution will start and end here.
 //
 
 #include <iostream>
